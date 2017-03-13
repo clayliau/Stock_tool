32c1d444762c12abed705f7998a50fa5d6963586
Clay Liao <clayliau@gmail.com>
2017-03-13 23:18:06 +0800

    1. Change trade volume into BigInteger for experiement 2. Change the bug in createSingleStock(data) 3. Add test code to write data into database

--- DB_Setup.py
+++ DB_Setup.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
-from sqlalchemy.types import Boolean, Date, Float
+from sqlalchemy.types import Boolean, Date, Float, BigInteger
 
 Base = declarative_base()
 
@@ -22,14 +22,15 @@ class SingleStockDaily(Base):
     
     id = Column(Integer, primary_key = True)
     tradeDate = Column(Date())
-    tradeVolume = Column(Integer)
-    tradeValue = Column(Integer)
+    tradeVolume = Column(BigInteger)
+    tradeValue = Column(BigInteger)
     openingprice = Column(Float())
     highestprice = Column(Float())
     lowestprice = Column(Float())
     closingtprice = Column(Float())
     change = Column(Float())
     transaction = Column(Integer)
+    stockid = Column(Integer)
 
 engine = create_engine('sqlite:///taiwanstock.db')
 Base.metadata.create_all(engine)
